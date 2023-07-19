9ff88bcd3600a59176e01759db4a1fd0c4308814
Balaji Jr <babubalaji877841@gmail.com>
2023-07-20 00:01:49 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ const App=()=>{
     );*/
 
     //Using Props
-    const Box=(props)=>{
+    /*const Box=(props)=>{
         return (
             <div className="box">
                 <h1 id='abc'>{props.heading}</h1>
@@ -98,4 +98,51 @@ const App=()=>{
         ReactDOM.render(
             <App />,
             document.getElementById('react-container')
-        );
+        );*/
+
+//Class component and states
+/*class Box extends React.Component{
+    state={
+        color:"black"
+    };
+    changeColor(color)
+    {
+        this.setState({color:color});
+    };
+    render(){
+    return (
+        <div className="box">
+            <h1 className={this.state.color}>{this.props.heading}</h1>
+            <p>This is a paragraph</p>
+            <button onClick={()=>this.changeColor("red")}>Red</button>
+            <button onClick={()=>this.changeColor("blue")}>Blue</button>
+            <button onClick={()=>this.changeColor("yellow")}>Yellow</button>
+            <button onClick={()=>this.changeColor("green")}>Green</button>
+        </div>
+    );
+    }
+    //()=> is being added to make it into a anonymous function.If not added this. will call it
+    //immediatly which should not happen 
+};
+const App=()=>{
+    return(
+        <div className='row'>
+            <div className='col'>
+                <Box heading='First Heading'/>
+            </div>
+            <div className='col'>
+                <Box heading='Second Heading'/>
+            </div>
+            <div className='col'>
+                <Box heading='Third Heading'/>
+            </div>
+            <div className='col'>
+                <Box heading='Fourth Heading'/>
+            </div>
+        </div>
+    );
+};
+    ReactDOM.render(
+        <App />,
+        document.getElementById('react-container')
+    );*/
